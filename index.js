bf93eb0eda3ce361e958e3ed1c8be6d1febfb5e7
Suyel Haque <slsuyel@gmail.com>
2023-04-28 23:18:48 +0600

    add

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-const express = require('express')
-const app = express()
-var cors = require('cors')
+const express = require("express");
+const app = express();
+var cors = require("cors");
 
-const port = 3000
+const port = 3000;
 
-const categories = require ('./data/categories.json')
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
+app.use(cors());
 
-app.use(cors())
+app.get("/", (req, res) => {
+  res.send("hello dragon news loading");
+});
 
-app.get('/',(req,res)=>{
-    res.send('hello dragon news loading')
-})
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
 
-app.get('/categories',(req,res)=>{
-    res.send(categories)
-})
+app.get("/news", (req, res) => {
+  res.send(news);
+});
 
-app.listen(port,()=>{
-    console.log(`dragon is comming ${port}`);
-})
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  // console.log(id);
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id)
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+    res.send(categoryNews);
+  }
+});
+app.listen(port, () => {
+  console.log(`dragon is comming ${port}`);
+});
